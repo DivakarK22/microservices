bbc37407af660d3ddc407fc2488f47b47055ded7
Your Name <you@example.com>
2023-11-16 14:17:33 -0500

    Test

--- client.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/bin/sensu-client -c /etc/sensu/config.json -l /var/log/sensu/sensu-client.log
--- uchiwa/server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/bin/sensu-server -c /etc/sensu/config.json -l /var/log/sensu/sensu-server.log

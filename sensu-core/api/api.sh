02e5325686eedc11be55c23978dcf97879766ee9
Your Name <you@example.com>
2023-11-16 14:26:50 -0500

    init

--- sensu-core/api/api.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/bin/sensu-api -c /etc/sensu/config.json -l /var/log/sensu/sensu-api.log -v
--- sensu-core/client/start-service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#/bin/sensu-server -c /etc/sensu/config.json -d /etc/sensu/conf.d -e /etc/sensu/extensions -l /var/log/sensu/sensu-client.log -b && /bin/sensu-api -c /etc/sensu/config.json -d /etc/sensu/conf.d -e /etc/sensu/extensions -l /var/log/sensu/sensu-client.log -b
-
-/bin/sensu-server -c /etc/sensu/config.json -l /var/log/sensu/sensu-server.log -b
-
-/bin/sensu-api -c /etc/sensu/config.json -l /var/log/sensu/sensu-api.log -b
-
-/bin/sensu-client -c /etc/sensu/config.json -l /var/log/sensu/sensu-client.log -b

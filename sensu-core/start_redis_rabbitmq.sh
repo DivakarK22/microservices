b25bf1a4a95ad65cbfc2e369701f547d2d7a470e
Your Name <you@example.com>
2023-11-18 04:12:12 +0530

    added

--- sensu-core/start_redis_rabbitmq.sh
+++ sensu-core/start_redis_rabbitmq.sh
@@ -8,6 +8,5 @@ docker run -d --name sensu-rabbitmq -p 5672:5672 -p 15672:15672 \
 docker run -d --name sensu-redis -p 6379:6379 redis:6.2
 
 docker exec -it sensu-rabbitmq rabbitmqctl add_user sensu sensu
-docker exec -it sensu-rabbitmq rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"
 
-docker ps
+docker exec -it sensu-rabbitmq rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"

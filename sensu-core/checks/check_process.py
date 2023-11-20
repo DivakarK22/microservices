c49fa14dd79e13bbfbd4a99b2168ab7ef1a9f562
Your Name <you@example.com>
2023-11-20 13:23:52 -0500

    Updated script

--- sensu-core/checks/check_process.py
+++ sensu-core/checks/check_process.py
@@ -1,18 +1,30 @@
-import subprocess
+import psutil
 import sys
+def checkIfProcessRunning(processName):
+    '''
+    Check if there is any running process that contains the given name processName.
+    '''
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False;
 
-def check_process(process_name):
-    try:
-        output = subprocess.check_output(["pgrep", "-f", process_name])
-        process_count = len(output.decode().splitlines())
-        if process_count > 0:
-            print(f"Process '{process_name}' is running")
-            return True
-        else:
-            print(f"No process found with name '{process_name}'")
-            return False
-    except subprocess.CalledProcessError:
-        print("Error executing the command")
-        sys.exit(2)
-process_name_to_check = sys.argv[1]
-check_process(process_name_to_check)
+if len(sys.argv) > 1:
+    process_name_to_check = sys.argv[1]
+    if checkIfProcessRunning(process_name_to_check):
+        print(f"The process '{process_name_to_check}' is running.")
+    else:
+        print(f"The process '{process_name_to_check}' is not running.")
+else:
+    print("Please provide a process name as a command-line argument.")
+
+#if checkIfProcessRunning(sys.argv) > 1:
+#    
+#    print('Yes a chrome process was running')
+#else:
+#    print('No chrome process was running')

e39cce956615b56d7b0a8fb98024f847458c98a2
Your Name <you@example.com>
2024-02-06 15:48:18 +0530

    added

--- sensu-core/checks/check-space.sh
+++ sensu-core/checks/check-space.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 Path=$1
+value=$2
 percentage=$(df -Th $Path | head -n 2 | tail -n 1 | cut -c 49-50)
 
-if [ "$percentage" -ge 17 ]; then
+if [ "$percentage" -ge $value ]; then
     echo "Disk Space in $Path is Higher than 17"
     exit 2
 else

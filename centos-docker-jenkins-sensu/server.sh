d214f09b9dd1ba0338ea597d9e81df9b844d2a6b
Divakar K <dvkr22@gmail.com>
2024-03-09 22:07:17 +0530

    added

--- centos-docker-jenkins-sensu/server.sh
+++ centos-docker-jenkins-sensu/server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/bin/sensu-client -c /etc/sensu/config.json -l /var/log/sensu/sensu-client.log -b
+/bin/sensu-client -c /etc/sensu/config.json -l /var/log/sensu/sensu-client.log -b && /opt/tomcat/bin/catalina.sh run
